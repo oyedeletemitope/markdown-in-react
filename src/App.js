4055c01120e4e1a4ad27f8460314ab93ac54d8ba
oyedeletemitope <meettopsy5fc@gmail.com>
2021-12-18 12:21:28 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { Remarkable } from "remarkable";
+
+const md = new Remarkable();
 
 function App() {
+  const [text, setText] = useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <main className="p-5 md:max-w-4xl md:mx-auto">
+        <h1 className="text-gray-900 text-4xl text-center font-bold">
+          markdown editor
+        </h1>
+
+        <article>
+          <label htmlFor="markdown" className="mt-5 mb-3 block"> type in some markdown</label>
+          <textarea
+            name="textarea"
+            id="markdown"
+            cols="30"
+            rows="10"
+            required
+            placeholder="type in some markdown"
+            className="bg-white p-5 rounded shadow w-full"
+            value={text}
+            onChange={(e)=>setText(e.target.value)}
+          ></textarea>
+
+          <h3>output</h3>
+          <div dangerouslySetInnerHTML={{__html:md.render(text)}}></div>
+        </article>
+      </main>
+    </>
   );
 }
 
